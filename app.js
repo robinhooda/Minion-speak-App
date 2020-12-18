55e92d4e70068ce095e11000a015398763f0f9e1
robinhooda <robinhooda66@gmail.com>
2020-12-18 21:15:36 +0530

    Text displayed to output box when btn clicked

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 var inputText = document.querySelector("#txt-input");
 var translateButton = document.querySelector("#btn-translate");
-
-console.log(inputText);
-console.log(translateButton);
+var outputText = document.querySelector("#txt-output");
 
 translateButton.addEventListener("click", clickEventListener)
 
-function clickEventListener(){
-    console.log("clicked");
+function clickEventListener() {
+    outputText.innerText = inputText.value;
 }
