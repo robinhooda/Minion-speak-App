ad349ccdb6cadadbf137fbd527b7dacf8f0391b0
robinhooda <robinhooda66@gmail.com>
2020-12-18 20:48:03 +0530

    Add: Eventlistener to btn

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("hiiii");
+var inputText = document.querySelector("#txt-input");
+var translateButton = document.querySelector("#btn-translate");
+
+console.log(inputText);
+console.log(translateButton);
+
+translateButton.addEventListener("click", clickEventListener)
+
+function clickEventListener(){
+    console.log("clicked");
+}
